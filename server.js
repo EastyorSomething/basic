c5f8c882d83d367e90c28e718e4200ced4237f2b
EastyorSomething <40967511+EastyorSomething@users.noreply.github.com>
2019-08-20 11:38:52 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var app = express();
 var data = "nothing"
-const net = require('net');
-const path = require('path');
 
  app.get('/', function(req, res){
-  //res.sendFile(__dirname+"/index.html");
+  //res.sendFile(__d(irname+"/index.html");
     //res.send(
 		//"<html> <head> <title>Snap Cloud Variables</title> </head> <body>"+query+"</body> </head>"
 	//)
@@ -15,6 +13,6 @@ const path = require('path');
 	}
 });
 
-net.createServer().listen( path.join('\\\\?\\pipe', process.cwd(), 'myctl')), function(){
-	console.log("Server is running");
-};
+app.listen(3000, function(){
+	console.log("Server is running on 3000");	
+});
